66c12c990ade115c5b1ad84f80bb3dfd03a88277
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2024-10-02 01:19:13 +0530

    adding splash to our app

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+export default Home;
--- nativewind-env.d.ts
+++ nativewind-env.d.ts
@@ -1,1 +1,3 @@
+/* eslint-disable prettier/prettier */
+// nativewind-env.d.ts
 /// <reference types="nativewind/types" />
