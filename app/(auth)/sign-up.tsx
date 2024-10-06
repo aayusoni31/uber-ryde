4ef48582981d5ced50a646eef81e38fd84df8e4f
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2024-10-06 17:20:19 +0530

    .

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,9 +1,17 @@
 /* eslint-disable prettier/prettier */
-
+import { Link, router } from "expo-router";
 import { Image, ScrollView, Text, View, StyleSheet } from "react-native";
-import { images } from "@/constants";
+import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import { useState } from "react";
 const SignUp = () => {
+  const [form, setform] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const onSignUpPress = async () => {};
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -14,14 +22,48 @@ const SignUp = () => {
           </Text>
         </View>
         <View className="p-5">
-          <InputField 
-          label="Name"
-          placeholder="ENter your Name"
-          icon ={icons.person}
-          value={}>
+          <InputField
+            label="Name"
+            placeholder="Enter your Name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign-up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+          {/* Oauth- that will allow us to login with google  */}
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200  mt-10"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
         </View>
+        {/* verification modal  */}
       </View>
     </ScrollView>
   );
 };
+
 export default SignUp;
--- components/InputField.tsx
+++ components/InputField.tsx
@@ -4,18 +4,49 @@ import {
   TouchableWithoutFeedback,
   View,
   Text,
+  Image,
+  Platform,
+  Keyboard,
+  TextInput,
 } from "react-native";
-
-const InputField = ({ label, labelStyle }) => {
-  // eslint-disable-next-line no-unused-expressions
-  <KeyboardAvoidingView>
-    <TouchableWithoutFeedback>
-      <View className="my-2 w-full">
-        <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
-          {label}
-        </Text>
-      </View>
-    </TouchableWithoutFeedback>
-  </KeyboardAvoidingView>;
+import { InputFieldProps } from "@/types/type";
+import { IOS } from "nativewind/dist/utils/selector";
+const InputField = ({
+  label,
+  labelStyle,
+  icon,
+  secureTextEntry = false,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  className,
+  ...props
+}: InputFieldProps) => {
+  return (
+    // eslint-disable-next-line no-unused-expressions
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="my-2 w-full">
+          <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
+            {label}
+          </Text>
+          <View
+            className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
+          >
+            {icon && (
+              <Image source={icon} className={`w-6 h-6 ml-4 $(iconStyle)`} />
+            )}
+            <TextInput
+              className={`rounded-full font-JakartaSemiBold p-4 text-[15px] flex-1 ${inputStyle} text-left`}
+              secureTextEntry={secureTextEntry}
+              {...props}
+            ></TextInput>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 };
 export default InputField;
